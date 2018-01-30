5d1d52bb922f56eab18406dcd4055c10cd211797
ignacio.lavina <lavina.ignacio@gmail.com>
2018-01-30 17:47:33 +0100

    creating frst methods

--- BlockChain.js
+++ BlockChain.js
@@ -1,5 +1,46 @@
-public class BlochChain{
-	
 
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+	constructor(index, transaction, otherProperties, previousHash){
+		this.index = index;
+		this.transaction = transaction;
+		this.otherProperties = otherProperties;
+		this.previousHash = previousHash;
+
+		this.none = '';
+	}
+
+	calculateHash(){
+		return SHA256(this.index + this.transaction + this.previousHash);
+	}
+
+
+}
+
+class Transaction{
+
+	constructor(seller, buyer, price, amount){
+		this.seller = seller;
+		this.buyer = buyer;
+		this.price = price;
+		this.amount = amount;
+
+	}
+
+	constructor(someDataString){
+		this.someDataString = someDataString;
+	}
+}
+
+class BlochChain{
 	
+	constructor(){
+		this.chain = [];
+	}
+
+	genesisBlock(){
+		return new Block(0, )
+	}
+
 }
