f4f94aee468bea4a1302a2df07f3d7e4bc53c909
ignacio.lavina <lavina.ignacio@gmail.com>
2018-02-01 23:30:32 +0100

    Changed Block constructor Fixed error on getLatestBlock function

--- main.js
+++ main.js
@@ -1,22 +1,61 @@
-
 const SHA256 = require('crypto-js/sha256');
 
 class Block{
-	constructor(index, transaction, otherProperties, previousHash){
+	constructor(index, data, previousHash = ''){
 		this.index = index;
-		this.transaction = transaction;
-		this.otherProperties = otherProperties;
+		this.data = data;
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
 
+//		this.transaction = transaction;
+//		this.otherProperties = otherProperties;
 		this.none = '';
 	}
 
 	calculateHash(){
-		return SHA256(this.index + this.transaction + this.previousHash);
+		return SHA256(this.index + this.previousHash + JSON.stringify(this.data)).toString();
 	}
 
 }
+
+
+
+class BlochChain{
+	
+	constructor(){
+		this.chain = [this.genesisBlock()];
+	}
+
+	getLatestBlock(){
+		return this.chain[this.chain.length - 1];
+	}
+
+	genesisBlock(){
+		let b = new Block(0, "Uco manda 10 bits a Klei", "0");
+		return b;
+	}
+
+	addBlock(newBlock){
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+
+	}
+
+//	addBlock(newBlock){
+//		newBlock.previousHash = this.getLatestBlock().hash;
+//		newBlock.hash = newBlock.calculateHash();
+//		this.chain.push(newBlock);
+//	}
+
+}
+
+
+let prueba = new BlochChain();
+prueba.addBlock(new Block(1, {amount: 10}));
+prueba.addBlock(new Block(2, {amount: 55}));
+console.log(JSON.stringify(prueba, null, 4));
+
 
 class Transaction{
 
@@ -37,29 +76,3 @@ class Transaction{
 		return this.someDataString;
 	}
 }
-
-class BlochChain{
-	
-	constructor(){
-		this.chain = [this.genesisBlock()];
-	}
-
-	getLatestBlock(){
-		return this.chain[this.length - 1];
-	}
-
-	genesisBlock(){
-		return new Block(0, new Transaction("Uco manda 10 bits a Klei"), '', '0');
-	}
-
-	addBlock(newBlock){
-		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
-		this.chain.push(newBlock);
-	}
-
-}
-
-let prueba = new BlochChain();
-prueba.addBlock(new Block(1,"bloque prueba","sin propiedades",this.getLatestBlock.hash));
-console.log(JSON.stringify(prueba, null, 4));
